d1511258e4be574308a116363e7df3cee542cf55
camilia-bouda <camilia.bouda@protonmail.com>
2022-09-03 23:13:06 +0200

    MaJ - Carte Interractive

--- 01_🚲_Pycyle.py
+++ 01_🚲_Pycyle.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import pytz
+from datetime import datetime
+import gdown
+from streamlit_plotly_mapbox_events import plotly_mapbox_events
 
-st.set_page_config(page_title = "Pycycle")
+st.set_page_config(page_title = "Pycycle", layout="wide")
 
 header = st.container()
 app = st.container()
@@ -12,34 +16,78 @@ with header:
     st.title("Pycycle")
     st.caption("Camilia Bouda, Gilles Schenfele")
     st.markdown("#### Découvrez l'état du trafic cycliste aujourd'hui ! :bicyclist:")
+    st.write("Sélectionner un compteur sur la carte pour voir son affluence habituelle au fil de la journée ⬇️")
 
 with app:
-    url = "https://drive.google.com/file/d/1Lsy7Hcjv52H4jrvTNgqznc-hw-mdhljN/view?usp=sharing"
-    url = 'https://drive.google.com/uc?id=' + url.split('/')[-2]
-    token = "pk.eyJ1IjoiY2FtaWxpYWIiLCJhIjoiY2w3a284am1uMDg5ejNvdDV6cWNzdTFsaSJ9.7nOYlVU0P_oLhl-7BnIu6Q"
+ 
+    @st.cache()
+    def load_data(url):
+        url = 'https://drive.google.com/uc?id=' + url.split('/')[-2]
+        df = pd.read_csv(url)
 
-    df = pd.read_csv(url)
-    df["Comptage horaire"] = df["Comptage horaire"].apply(lambda x: round(x,0))
+        return df
     
-    fig = px.scatter_mapbox(df, 
+    token = "pk.eyJ1IjoiY2FtaWxpYWIiLCJhIjoiY2w3a284am1uMDg5ejNvdDV6cWNzdTFsaSJ9.7nOYlVU0P_oLhl-7BnIu6Q"
+    affluence_compteur = load_data("https://drive.google.com/file/d/1-C2wCqL-A0Z07oxqoVSjKk3QZYG8wwcF/view?usp=sharing")
+    affluence_compteur = affluence_compteur.rename(columns={'Comptage horaire': 'Comptage horaire moyen'})
+    affluence_compteur = affluence_compteur.reset_index(drop=False)
+
+    fig = px.scatter_mapbox(affluence_compteur, 
                             lat="Lat",
                             lon="Long", 
-                            size="Comptage horaire", 
-                            color = "Comptage horaire",
+                            size="Comptage horaire moyen", 
+                            color = "Comptage horaire moyen",
                             hover_name = "Nom du compteur",
-                            size_max=35,
-                            zoom=11
+                            hover_data=["index"],
+                            size_max=30,
+                            zoom=10.5,
+                            title="Affluence horaire habituelle",
                             )
 
     fig.update_layout(mapbox_style="light", mapbox_accesstoken=token)
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-    fig.update_layout(width = 800, height = 500)
+    fig.update_layout(height = 400)
 
-    st.write(fig)
+    plot_name_holder_clicked = st.empty()
+
+    mapbox_events = plotly_mapbox_events(
+        fig,
+        click_event=True,
+        select_event=False,
+        hover_event=False,
+        relayout_event=False,
+        override_width = "100%")
+
+    compteurs = affluence_compteur[['index', 'Nom du compteur']]
+    selected_compteur_index = mapbox_events[0][0]['pointIndex']
+    selected_compteur_name = affluence_compteur.loc[selected_compteur_index, 'Nom du compteur']
+
+    
+
+    affluence_heure = load_data("https://drive.google.com/file/d/1aFA3dbK4VSK2h79UIgM9yTXzpO9gzf2h/view?usp=sharing")
+    affluence_heure = affluence_heure.reset_index(drop=False)
+
+    df2 = affluence_heure[affluence_heure['Nom du compteur'] == selected_compteur_name]
+    fig2 = px.bar(df2, x="Heure", y="Comptage horaire", hover_data=['Nom du compteur', 'Affluence'])
+    fig2.update_layout(paper_bgcolor="rgba(0,0,0,0)", plot_bgcolor="rgba(0,0,0,0)")
+    fig2.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig2.update_xaxes(ticksuffix="h")
+    fig2.update_layout(height = 300)
+
+    tz = pytz.timezone('Europe/Paris')
+    now = datetime.now(tz).hour
+    affluence = str(df2.loc[df2['Heure'] == 20]['Affluence']).split('\n')[0].split('  ')[-1]
+    texte = str(now) + 'h: Habituellement **' + affluence.lower() + "**"
+
+    st.markdown("Compteur: " + selected_compteur_name)
+    st.markdown(texte)
+    st.plotly_chart(fig2, use_container_width=True)
+
+
 
 with dataviz:
     st.markdown("#### Explorer les données ! :rocket:")
-
+    
 
 
 
--- pages/02_🤖_Predictions.py
+++ pages/02_🤖_Predictions.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import pandas as pd
+from joblib import load
+import gdown
+import plotly.express as px
+
+
 
 st.set_page_config(page_title = "Pycycle")
 
@@ -6,3 +12,162 @@ header = st.container()
 
 with header:
     st.title("Modèles prédictifs")
+
+    tab_global1, tab_global2 = st.tabs(["Prévision du trafic global horaire", "Prévision du trafic par capteur"])
+
+    with tab_global1:
+        st.markdown("#### Prévision du niveau de trafic global de la ville par heure")
+        st.text("Régressions établies sur les données horaires agrégées disponibles depuis 2019")
+        st.text("4 modèles ont été entraînés et les scores (r2 et MAE) obtenus après\n plusieurs itérations sont les suivants :")
+        
+
+        
+        
+        ### data du modele (dataset entrainement et dataset de test)
+        url1 = "https://drive.google.com/file/d/1h_JNUIUM3dtvWVnd8aRpmt-HDWFB8pv5/view?usp=sharing"
+        url1 = 'https://drive.google.com/uc?id=' + url1.split('/')[-2]
+        output1 = "file1.csv"
+        gdown.download(url1, output1, quiet=False) 
+        df_Hour_count = pd.read_csv(output1, sep=";", index_col=0)
+        
+        df_Hour_count = df_Hour_count.sort_values(by = 'Date', ascending = True)
+        last_training_index = int(round(df_Hour_count.shape[0]*0.8,0))
+        train_set = df_Hour_count.iloc[0:last_training_index, :]
+        test_set = df_Hour_count.iloc[last_training_index:,:]
+        
+        X_train = train_set.drop(['Comptage horaire', 'Date'], axis = 1)
+        y_train = train_set['Comptage horaire']
+        
+        X_test = test_set.drop(['Comptage horaire', 'Date'], axis = 1)
+        y_test = test_set['Comptage horaire']      
+
+        ### dataframe avec les résultats
+        url = "https://drive.google.com/file/d/1vSWvb_q9C2Xfh0bnoMQm5FDCkM-N5CfU/view?usp=sharing"
+        url = 'https://drive.google.com/uc?id=' + url.split('/')[-2]
+        output = "file.csv"
+        gdown.download(url, output, quiet=False) 
+        df_score_model_global = pd.read_csv(output, sep=";", index_col=0)
+        
+        st.table(df_score_model_global)
+    
+        tab1, tab2, tab3, tab4 = st.tabs(["Lasso", "Ridge", "Elastic Net CV", "Random Forest"])
+    
+        with tab1:
+            st.markdown("#### Régression Lasso")
+            
+            url = "https://drive.google.com/file/d/1--uJQdiPpAsOf2IeSOJ5Jt781dqoA2p-/view?usp=sharing"
+            url = "https://drive.google.com/uc?id=" + url.split('/')[-2]
+            output = "model.joblib"
+            gdown.download(url, output, quiet=False)
+            model_global_lasso = load(output)
+            
+            lasso_pred_train = model_global_lasso.predict(X_train)
+            lasso_pred_test = model_global_lasso.predict(X_test)
+            
+            st.text("score sur le dataset d'entraînement : " +  str(model_global_lasso.score(X_train,y_train)))
+            st.text("score sur le dataset de test : " + str(model_global_lasso.score(X_test,y_test)))
+        
+            
+        
+            fig = px.scatter(x = train_set['Date'],
+                             y = lasso_pred_train)
+            fig2 = px.bar(x = train_set['Date'],
+                          y = y_train)
+            fig.add_trace(fig2.data[0])
+            fig.update_layout(width = 800, height = 500)
+            fig.update_layout(title= "Fitting sur le dataset d'entraînement")
+            st.write(fig)
+            
+            
+            fig = px.scatter(x = test_set['Date'],
+                             y = lasso_pred_test)
+            fig2 = px.bar(x = test_set['Date'],
+                          y = y_test)
+            fig.add_trace(fig2.data[0])
+            fig.update_layout(width = 800, height = 500)
+            fig.update_layout(title= "Prévisions sur le dataset de test")
+            st.write(fig)
+            
+        
+        with tab2:
+            st.markdown("#### Régression Ridge")
+
+            #url = format(str('https://drive.google.com/file/d/1-5d4mOyc-Wp3uERBOdML9MsxdMAAzZF5/view?usp=sharing'))
+            #url = 'https://drive.google.com/uc?id=' + url.split('/')[-2] 
+            #model_global_ridge = load(url)
+            #st.text(model_global_ridge.score)
+        
+        with tab3:
+            st.markdown("#### Régression Elastic Net CV")
+
+            #url = "https://drive.google.com/file/d/1-A-Ipr2WjKjtxIvQ3k6Z9ANXVbgirWP3/view?usp=sharing"
+            #url = "https://drive.google.com/uc?id=" + url.split('/')[-2] 
+            #model_global_lasso = load(url)
+            #st.text(model_global_lasso.score)
+            
+        with tab4:
+            st.markdown("#### Régression Random Forest")
+
+            #url = "https://drive.google.com/file/d/1-ANJZ8FZnR5GLw1FyDTSF6Y0QocIGHNp/view?usp=sharing"
+            #url = "https://drive.google.com/uc?id=" + url.split('/')[-2] 
+            #model_global_lasso = load(url)
+            #st.text(model_global_lasso.score)
+        
+    
+    with tab_global2:
+        st.markdown("#### Prévision du niveau de trafic par capteur, par tranche horaire")
+        st.text("Régressions établies par capteur et tranche de 4 heures depuis 2019")
+        st.text("4 modèles ont été entraînés et les scores (r2 et MAE) obtenus après\n plusieurs itérations sont les suivants :")
+        
+
+        
+        
+        ### data du modele (dataset entrainement et dataset de test)
+        url2 = "    "
+        url2 = 'https://drive.google.com/uc?id=' + url.split('/')[-2]
+        output2 = "file2.csv"
+        gdown.download(url2, output2, quiet=False)        
+        df_capteur_tr_horaire = pd.read_csv(output2, sep=";", index_col=0)
+        
+        df_capteur_tr_horaire = df_capteur_tr_horaire.sort_values(by = 'Date', ascending = True)
+        df_capteur_tr_horaire = df_capteur_tr_horaire.dropna()
+        last_training_index = int(round(df_capteur_tr_horaire.shape[0]*0.8,0))
+        train_set = df_capteur_tr_horaire.iloc[0:last_training_index, :]
+        test_set = df_capteur_tr_horaire.iloc[last_training_index:,:]
+        
+        X_train = train_set.drop(['Comptage Tranche Horaire', 'Date'], axis = 1)
+        y_train = train_set['Comptage Tranche Horaire']
+        
+        X_test = test_set.drop(['Comptage Tranche Horaire', 'Date'], axis = 1)
+        y_test = test_set['Comptage Tranche Horaire']     
+
+
+        ### dataframe avec les résultats
+        url = "https://drive.google.com/file/d/1-EiGcmrlx4EigIt71JzvR-uiEaJnszlu/view?usp=sharing"
+        url = 'https://drive.google.com/uc?id=' + url.split('/')[-2]
+        output = "file.csv"
+        gdown.download(url, output, quiet=False)
+        df_score_model_capteur = pd.read_csv(output, sep=";", index_col=0)
+        
+        st.table(df_score_model_capteur)
+    
+        tab1, tab2, tab3, tab4 = st.tabs(["Lasso", "Ridge", "Elastic Net CV", "Random Forest"])
+    
+        with tab1:
+            st.markdown("#### Régression Lasso")
+            
+        
+        
+        with tab2:
+            st.markdown("#### Régression Ridge")
+
+
+        
+        with tab3:
+            st.markdown("#### Régression Elastic Net CV")
+
+
+            
+        with tab4:
+            st.markdown("#### Régression Random Forest")
+
