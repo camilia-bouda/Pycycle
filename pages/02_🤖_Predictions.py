bb176429f2a46959201922ca12328646981d070a
GillesSch <auguste.jaffrelot@gmail.com>
2022-09-13 00:22:18 +0200

    220912 save page 2 modele

--- pages/02_🤖_Predictions.py
+++ pages/02_🤖_Predictions.py
@@ -101,84 +101,49 @@ with header:
                 
         
         ### data du modele horaire agrégé à la ville (dataset entrainement et dataset de test)
-        df_Hour_count = gdown_csv("https://drive.google.com/file/d/1h_JNUIUM3dtvWVnd8aRpmt-HDWFB8pv5/view?usp=sharing")
+        df_Hour_count = gdown_csv("https://drive.google.com/file/d/1tSDX-jjYRku5hNWNkXTKM6hyC25KcuW1/view?usp=sharing")
         
         ### split des datas
         train_set_Hour_count, test_set_Hour_count, X_train_Hour_count, y_train_Hour_count, X_test_Hour_count, y_test_Hour_count = traintestsplit_df_Hour_count(df_Hour_count)
     
 
         ### dataframe avec les résultats
-        df_score_model_global = gdown_csv("https://drive.google.com/file/d/1vSWvb_q9C2Xfh0bnoMQm5FDCkM-N5CfU/view?usp=sharing")
+        df_score_model_global = gdown_csv("https://drive.google.com/file/d/1-AeeyzgZ70KBuS_RVceOajL7wTIabhlf/view?usp=sharing")
         df_score_model_global[["Score_train","Score_test"]] = round(df_score_model_global[["Score_train","Score_test"]],4)
         df_score_model_global[["MAE_train","MAE_test"]] = round(df_score_model_global[["MAE_train","MAE_test"]])
         df_score_model_global = df_score_model_global.astype({"MAE_train" : int,
                                                               "MAE_test" : int})
         
+        
         st.table(df_score_model_global)
-    
-        tab1, tab2, tab3, tab4 = st.tabs(["Lasso", "Ridge", "Elastic Net CV", "Random Forest"])
-    
-        with tab1:
-            st.markdown("#### Régression Lasso")
+        
+        st.markdown("#### Modèle Random Forest")
+        
+        
+        if st.button('Charger le modele'): 
+        
+            model_global_RF = gdown_joblib("https://drive.google.com/file/d/1pt4Dg9MokgAUrhlaR8codl-3WScaSjDv/view?usp=sharing")
+            RF_pred_train, RF_pred_test = calcul_predictions(model_global_RF, X_train_Hour_count, X_test_Hour_count)
             
-            model_global_lasso = gdown_joblib("https://drive.google.com/file/d/1--uJQdiPpAsOf2IeSOJ5Jt781dqoA2p-/view?usp=sharing")
-            lasso_pred_train, lasso_pred_test = calcul_predictions(model_global_lasso, X_train_Hour_count, X_test_Hour_count)
-            
-            st.write("score sur le dataset d'entraînement : " +  str(round(model_global_lasso.score(X_train_Hour_count,y_train_Hour_count),4)))
-            st.write("score sur le dataset de test : " + str(round(model_global_lasso.score(X_test_Hour_count,y_test_Hour_count),4)))
+            st.write("score sur le dataset d'entraînement : " +  str(round(model_global_RF.score(X_train_Hour_count,y_train_Hour_count),4)))
+            st.write("score sur le dataset de test : " + str(round(model_global_RF.score(X_test_Hour_count,y_test_Hour_count),4)))
         
             
             
-            fig_lasso_train = plot_prediction(train_set_Hour_count['Date'],
-                                              lasso_pred_train, y_train_Hour_count,
+            fig_RF_train = plot_prediction(train_set_Hour_count['Date'],
+                                              RF_pred_train, y_train_Hour_count,
                                               "Fitting sur le dataset d'entraînement")
-            st.write(fig_lasso_train)
-
-            fig_lasso_test = plot_prediction(test_set_Hour_count['Date'],
-                                              lasso_pred_test, y_test_Hour_count,
+            st.write(fig_RF_train)
+    
+            fig_RF_test = plot_prediction(test_set_Hour_count['Date'],
+                                              RF_pred_test, y_test_Hour_count,
                                               "Prévisions sur le dataset de test")
-            st.write(fig_lasso_test)            
+            st.write(fig_RF_test)            
             
-            
-        
-        with tab2:
-            st.markdown("#### Régression Ridge")
-
-            model_global_ridge = gdown_joblib("https://drive.google.com/file/d/1-5d4mOyc-Wp3uERBOdML9MsxdMAAzZF5/view?usp=sharing")
-            ridge_pred_train, ridge_pred_test = calcul_predictions(model_global_ridge, X_train_Hour_count, X_test_Hour_count)
-            
-            st.write("score sur le dataset d'entraînement : " +  str(round(model_global_ridge.score(X_train_Hour_count,y_train_Hour_count),4)))
-            st.write("score sur le dataset de test : " + str(round(model_global_ridge.score(X_test_Hour_count,y_test_Hour_count),4)))
-        
-            
-            
-            fig_ridge_train = plot_prediction(train_set_Hour_count['Date'],
-                                              ridge_pred_train, y_train_Hour_count,
-                                              "Fitting sur le dataset d'entraînement")
-            st.write(fig_ridge_train)
-
-            fig_ridge_test = plot_prediction(test_set_Hour_count['Date'],
-                                              ridge_pred_test, y_test_Hour_count,
-                                              "Prévisions sur le dataset de test")
-            st.write(fig_ridge_train)            
-
-        
-        with tab3:
-            st.markdown("#### Régression Elastic Net CV")
-
-            #url = "https://drive.google.com/file/d/1-A-Ipr2WjKjtxIvQ3k6Z9ANXVbgirWP3/view?usp=sharing"
-            #url = "https://drive.google.com/uc?id=" + url.split('/')[-2] 
-            #model_global_lasso = load(url)
-            #st.text(model_global_lasso.score)
-            
-        with tab4:
-            st.markdown("#### Régression Random Forest")
-
-            #url = "https://drive.google.com/file/d/1-ANJZ8FZnR5GLw1FyDTSF6Y0QocIGHNp/view?usp=sharing"
-            #url = "https://drive.google.com/uc?id=" + url.split('/')[-2] 
-            #model_global_lasso = load(url)
-            #st.text(model_global_lasso.score)
-        
+        else:
+            st.write("Charger le modèle Random Forest (env. 600Mo, temps de chargement d'une minute)")
+             
+    
     
     with tab_global2:
         st.markdown("#### Prévision du niveau de trafic par capteur, par tranche horaire")
@@ -188,6 +153,7 @@ with header:
 
         ### data du modele par capteur et tranches horaires (dataset entrainement et dataset de test)
         df_capteur_tr_horaire = gdown_csv("https://drive.google.com/file/d/1-2u6zMBGQBqGp1nRHpy7IQu1OO9C6YRq/view?usp=sharing")
+        
         
         ### split des datas
         train_set_capteur_tr_horaire, test_set_capteur_tr_horaire, X_train_capteur_tr_horaire, y_train_capteur_tr_horaire, X_test_capteur_tr_horaire, y_test_capteur_tr_horaire = traintestsplit_df_capteur_tr_horaire(df_capteur_tr_horaire)
@@ -204,61 +170,66 @@ with header:
                 
         st.table(df_score_model_capteur)
     
-        tab1, tab2, tab3, tab4 = st.tabs(["Lasso", "Ridge", "Elastic Net CV", "Random Forest"])
+   
+        st.markdown("#### Modèle Random Forest")
+        
+        
+        if st.button('Charger le modèle'):        
+            model_capteur_RF = gdown_joblib("https://drive.google.com/file/d/1-AQqkeCysg5BN2ZQoqz5J1i5Y1xA0uKZ/view?usp=sharing")
+            RF_pred_train, RF_pred_test = calcul_predictions(model_capteur_RF, X_train_capteur_tr_horaire, X_test_capteur_tr_horaire)
+            
+            st.write("score sur le dataset d'entraînement : " +  str(round(model_capteur_RF.score(X_train_capteur_tr_horaire,y_train_capteur_tr_horaire),4)))
+            st.write("score sur le dataset de test : " + str(round(model_capteur_RF.score(X_test_capteur_tr_horaire,y_test_capteur_tr_horaire),4)))
+
+        else:
+            st.write("Charger le modèle Random Forest (env. 4Go, temps de chargement de quelques minutes)")
+ 
+
+        st.write("Tranche 0 : 21h - 1h, Tranche 1 : 1h - 4h, Tranche 2 : 5h - 8h, Tranche 3 : 9h - 12h, Tranche 4 : 13h - 16h, Tranche 5 : 17h - 20h")
     
-        with tab1:
-            st.markdown("#### Régression Lasso")
-            
-            model_capteur_lasso = gdown_joblib("https://drive.google.com/file/d/1-4Tadbv-qLNnNJN8gDlMJK_f70n62Etp/view?usp=sharing")
-            lasso_pred_train, lasso_pred_test = calcul_predictions(model_capteur_lasso, X_train_capteur_tr_horaire, X_test_capteur_tr_horaire)
-            
-            st.write("score sur le dataset d'entraînement : " +  str(round(model_capteur_lasso.score(X_train_capteur_tr_horaire,y_train_capteur_tr_horaire),4)))
-            st.write("score sur le dataset de test : " + str(round(model_capteur_lasso.score(X_test_capteur_tr_horaire,y_test_capteur_tr_horaire),4)))
+        df_capteurs_index = gdown_csv("https://drive.google.com/file/d/1wJvqoWAaCG4x-UDNPppiva47vnsQnV0F/view?usp=sharing")
         
-            df_capteurs_index = gdown_csv("https://drive.google.com/file/d/1wJvqoWAaCG4x-UDNPppiva47vnsQnV0F/view?usp=sharing")
+        id_compteur = 0
+        
+        df_capteurs_index = df_capteurs_index[df_capteurs_index['id compteur'].isin(test_set_capteur_tr_horaire["id compteur"].unique())]
+        
+        select_capteur = st.selectbox('Choix du capteur (temps de calcul d\'une à deux minutes)', df_capteurs_index['Capteur'])
+        
+        id_compteur = df_capteurs_index.loc[df_capteurs_index['Capteur']==select_capteur, "id compteur"].values[0]
+        
+        df_fig_train = train_set_capteur_tr_horaire
+        df_fig_train["Prevision"] = pd.Series(RF_pred_train)
+        df_fig_train["Reel"] = y_train_capteur_tr_horaire
+        df_fig_train = df_fig_train[df_fig_train["id compteur"] == id_compteur]
+        df_fig_train["Tranche Horaire"] = (df_fig_train['Tranche horaire_1']*1 + df_fig_train['Tranche horaire_2']*2 + df_fig_train['Tranche horaire_3']*3 + df_fig_train['Tranche horaire_4']*4 + df_fig_train['Tranche horaire_5']*5)
+        df_fig_train = df_fig_train.sort_values(by = ['Date',"Tranche Horaire"], ascending = True)
+        ### drop duplicates car il y a un soucis dans le dataset avec les tranches historiques ?
+        # df_fig_train = df_fig_train.drop_duplicates(subset=["Date","Tranche Horaire"])
+        
+        fig_RF_train = plot_prediction(df_fig_train['Date - Tranche Horaire'],
+                                          df_fig_train['Prevision'], df_fig_train['Reel'],
+                                          "Fitting sur le dataset d'entraînement")
+        st.write(fig_RF_train)
+
+        df_fig_test = test_set_capteur_tr_horaire
+        df_fig_test["Prevision"] = pd.Series(RF_pred_test)
+        df_fig_test["Reel"] = y_test_capteur_tr_horaire
+        df_fig_test = df_fig_test[df_fig_test["id compteur"] == id_compteur]
+        df_fig_test["Tranche Horaire"] = (df_fig_test['Tranche horaire_1']*1 + df_fig_test['Tranche horaire_2']*2 + df_fig_test['Tranche horaire_3']*3 + df_fig_test['Tranche horaire_4']*4 + df_fig_test['Tranche horaire_5']*5)
+        df_fig_test = df_fig_test.sort_values(by = ['Date',"Tranche Horaire"], ascending = True)
+        ### drop duplicates car il y a un soucis dans le dataset avec les tranches historiques ?
+        # df_fig_test = df_fig_test.drop_duplicates(subset=["Date","Tranche Horaire"])
+        
+        if len(df_fig_test) == 0:
+            st.write("Ce capteur est absent du dataset de test")
             
-            id_compteur = 0
-            
-            select_capteur = st.selectbox('Capteur', df_capteurs_index['Capteur'])
-            
-            id_compteur = df_capteurs_index.loc[df_capteurs_index['Capteur']==select_capteur, "id compteur"].values[0]
-            
-            df_fig_train = train_set_capteur_tr_horaire
-            df_fig_train["Prevision"] = pd.Series(lasso_pred_train)
-            df_fig_train["Reel"] = y_train_capteur_tr_horaire
-            df_fig_train = df_fig_train[df_fig_train["id compteur"] == id_compteur]
-            
-            fig_lasso_train = plot_prediction(df_fig_train['Date - Tranche Horaire'],
-                                              df_fig_train['Prevision'], df_fig_train['Reel'],
+        else:
+        
+            fig_lasso_test = plot_prediction(df_fig_test['Date - Tranche Horaire'],
+                                              df_fig_test['Prevision'], df_fig_test['Reel'],
                                               "Fitting sur le dataset d'entraînement")
-            st.write(fig_lasso_train)
-
-            df_fig_test = test_set_capteur_tr_horaire
-            df_fig_test["Prevision"] = pd.Series(lasso_pred_test)
-            df_fig_test["Reel"] = y_test_capteur_tr_horaire
-            df_fig_test = df_fig_test[df_fig_test["id compteur"] == id_compteur]
-            
-            if len(df_fig_test) == 0:
-                st.write("Ce capteur est absent du dataset de test")
+            st.write(fig_lasso_test)   
                 
-            else:
-            
-                fig_lasso_test = plot_prediction(df_fig_test['Date - Tranche Horaire'],
-                                                  df_fig_test['Prevision'], df_fig_test['Reel'],
-                                                  "Fitting sur le dataset d'entraînement")
-                st.write(fig_lasso_test)           
         
-        
-        with tab2:
-            st.markdown("#### Régression Ridge")
 
-
-        
-        with tab3:
-            st.markdown("#### Régression Elastic Net CV")
-
-
-            
-        with tab4:
-            st.markdown("#### Régression Random Forest")
-
+    
