45648caa1c1f4295944c86e6281590ddf871ce65
camilia-bouda <camilia.bouda@protonmail.com>
2022-09-23 18:19:30 +0200

    Update - Détail du projet

---
+++ pages/02_🤖_Prediction du trafic cycliste.py
@@ -0,0 +1,225 @@
+import streamlit as st
+import pandas as pd
+from joblib import load
+import gdown
+import plotly.express as px
+from streamlit_plotly_mapbox_events import plotly_mapbox_events
+import json
+
+
+
+st.set_page_config(page_title = "Pycycle", layout="wide", initial_sidebar_state="collapsed", page_icon = ":bicyclist:")
+
+header = st.container()
+carte = st.container()
+graphe_carte = st.container()
+
+with header:
+    st.title("Modèles prédictifs")
+
+    tab_global1, tab_global2 = st.tabs(["Prévision du trafic global horaire", "Prévision du trafic par capteur"])
+    
+    @st.cache(allow_output_mutation=True)
+    def load_data(url):
+        url = 'https://drive.google.com/uc?id=' + url.split('/')[-2]
+        df = pd.read_csv(url)
+        return df
+    
+    @st.cache(allow_output_mutation=True)
+    def gdown_csv(url, separateur=";", index_colonne=0):
+        url = 'https://drive.google.com/uc?id=' + url.split('/')[-2]
+        output = "file.csv"
+        gdown.download(url, output, quiet=False) 
+        df = pd.read_csv(output, sep=separateur, index_col=index_colonne)
+        return df
+    
+    @st.cache(allow_output_mutation=True)
+    def gdown_joblib(url):
+        url = 'https://drive.google.com/uc?id=' + url.split('/')[-2]
+        output = "model.joblib"
+        gdown.download(url, output, quiet=False) 
+        model = load(output)
+        return model
+    
+    @st.cache(allow_output_mutation=True)
+    def traintestsplit_df_Hour_count(df_Hour_count):
+        df_Hour_count = df_Hour_count.sort_values(by = 'Date', ascending = True)
+        last_training_index = int(round(df_Hour_count.shape[0]*0.8,0))
+        train_set = df_Hour_count.iloc[0:last_training_index, :]
+        test_set = df_Hour_count.iloc[last_training_index:,:]
+        
+        X_train = train_set.drop(['Comptage horaire', 'Date'], axis = 1)
+        y_train = train_set['Comptage horaire']
+        
+        X_test = test_set.drop(['Comptage horaire', 'Date'], axis = 1)
+        y_test = test_set['Comptage horaire']
+        
+        return train_set, test_set, X_train, y_train, X_test, y_test
+
+    @st.cache(allow_output_mutation=True)
+    def traintestsplit_df_capteur_tr_horaire(df_capteur_tr_horaire):    
+        df_capteur_tr_horaire = df_capteur_tr_horaire.sort_values(by = 'Date', ascending = True)
+        df_capteur_tr_horaire = df_capteur_tr_horaire.dropna()
+        last_training_index = int(round(df_capteur_tr_horaire.shape[0]*0.8,0))
+        train_set = df_capteur_tr_horaire.iloc[0:last_training_index, :]
+        test_set = df_capteur_tr_horaire.iloc[last_training_index:,:]
+        
+        X_train = train_set.drop(['Comptage Tranche Horaire', 'Date'], axis = 1)
+        y_train = train_set['Comptage Tranche Horaire']
+        
+        X_test = test_set.drop(['Comptage Tranche Horaire', 'Date'], axis = 1)
+        y_test = test_set['Comptage Tranche Horaire']
+        
+        return train_set, test_set, X_train, y_train, X_test, y_test
+
+    @st.cache(allow_output_mutation=True)
+    def plot_prediction(x_data, pred_data, real_data, titre=""):
+        fig = px.bar(x = x_data,
+                     y = real_data,
+                     opacity=0.7,
+                     color_discrete_sequence=['grey'],
+                     labels={'x': 'Temps', 'y':'Volume'})
+        fig2 = px.scatter(x = x_data,
+                         y = pred_data)
+        fig.add_trace(fig2.data[0])
+        fig.update_layout(width = 800, height = 500)
+        fig.update_layout(title= titre)
+        
+        return fig
+
+
+    @st.cache(allow_output_mutation=True)
+    def calcul_predictions(model, X_train, X_test):
+        pred_train = model.predict(X_train)
+        pred_test = model.predict(X_test)        
+
+        return pred_train, pred_test
+
+
+
+
+
+    with tab_global1:
+        st.markdown("#### Prévision du niveau de trafic global de la ville par heure")
+        st.write("Régressions établies sur les données horaires agrégées disponibles depuis 2019")
+        st.write("4 modèles ont été entraînés et les scores (r2 et MAE) obtenus après plusieurs itérations sont les suivants :")
+                
+        ### dataframe avec les résultats
+        df_score_model_global = gdown_csv("https://drive.google.com/file/d/1-AeeyzgZ70KBuS_RVceOajL7wTIabhlf/view?usp=sharing")
+        df_score_model_global[["Score_train","Score_test"]] = round(df_score_model_global[["Score_train","Score_test"]],4)
+        df_score_model_global[["MAE_train","MAE_test"]] = round(df_score_model_global[["MAE_train","MAE_test"]])
+        df_score_model_global = df_score_model_global.astype({"MAE_train" : int,
+                                                              "MAE_test" : int})
+        
+        
+        st.table(df_score_model_global)
+    
+        
+        st.markdown("#### Modèle Random Forest")
+        
+        RF_pred_train = gdown_csv("https://drive.google.com/file/d/1W3sqegoj5jCmGAly-T_VNxr5qn57h5mp/view?usp=sharing")
+        RF_pred_test = gdown_csv("https://drive.google.com/file/d/1fScrX7drOsCjajpEyEuhRZgpc6vWv-vv/view?usp=sharing")
+        
+
+        fig_RF_train = plot_prediction(RF_pred_train['Date'],
+                                          RF_pred_train['prediction'], RF_pred_train['Comptage horaire'],
+                                          "Fitting sur le dataset d'entraînement")
+        st.write(fig_RF_train)
+
+        fig_RF_test = plot_prediction(RF_pred_test['Date'],
+                                          RF_pred_test['prediction'], RF_pred_test['Comptage horaire'],
+                                          "Prévisions sur le dataset de test")
+        st.write(fig_RF_test)            
+            
+    
+    with tab_global2:
+        st.markdown("#### Prévision du niveau de trafic par capteur, par tranche horaire")
+        st.write("Régressions établies par capteur et tranche de 4 heures depuis 2019")
+        st.write("4 modèles ont été entraînés et les scores (r2 et MAE) obtenus après plusieurs itérations sont les suivants :")
+        
+        
+        ### dataframe avec les résultats
+        df_score_model_capteur = gdown_csv("https://drive.google.com/file/d/1-EiGcmrlx4EigIt71JzvR-uiEaJnszlu/view?usp=sharing")
+        df_score_model_capteur[["Score_train","Score_test"]] = round(df_score_model_capteur[["Score_train","Score_test"]],4)
+        df_score_model_capteur[["MAE_train","MAE_test"]] = round(df_score_model_capteur[["MAE_train","MAE_test"]])
+        df_score_model_capteur = df_score_model_capteur.astype({"MAE_train" : int,
+                                                              "MAE_test" : int})
+                
+        st.table(df_score_model_capteur)
+    
+   
+        st.markdown("#### Modèle Random Forest")
+        
+        
+        RF_pred_train = gdown_csv("https://drive.google.com/file/d/1SR0Efyz0slwaXpT77zb52GUY0fwLTV-n/view?usp=sharing")
+        RF_pred_test = gdown_csv("https://drive.google.com/file/d/1--xQjipzWgI5J98zGjzu9G_LL71RERlk/view?usp=sharing")
+  
+
+
+    
+        df_capteurs_index = gdown_csv("https://drive.google.com/file/d/1wJvqoWAaCG4x-UDNPppiva47vnsQnV0F/view?usp=sharing")
+        id_compteur = 0
+        df_capteurs_index =  df_capteurs_index[df_capteurs_index['id compteur'].isin(RF_pred_test["id compteur"].unique())]
+        df_capteurs_index =  df_capteurs_index[df_capteurs_index['id compteur'].isin(RF_pred_train["id compteur"].unique())]
+        df_capteurs_index["size"] = 5
+
+        token = "pk.eyJ1IjoiY2FtaWxpYWIiLCJhIjoiY2w3a284am1uMDg5ejNvdDV6cWNzdTFsaSJ9.7nOYlVU0P_oLhl-7BnIu6Q"
+        
+
+
+        st.write("Choix du compteur")
+        
+        fig = px.scatter_mapbox(df_capteurs_index, 
+                                lat="Lat",
+                                lon="Long", 
+                                size="size", 
+                                hover_name = "Nom du compteur",
+                                size_max=10,
+                                zoom=11)
+
+        fig.update_layout(mapbox_style="light", mapbox_accesstoken=token)
+        fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+        fig.update_layout(height = 400)
+        fig.update_coloraxes(showscale=False)
+
+        mapbox_events = plotly_mapbox_events(
+                                            fig,
+                                            click_event=True,
+                                            select_event=False,
+                                            hover_event=False,
+                                            relayout_event=False,
+                                            override_width = "100%",
+                                            key = 'selected_bike_meter')
+
+    
+        if st.session_state.selected_bike_meter != "[]":  #If a bike meter has been selected, we show the graph of the hourly trafic
+            id_compteur = json.loads(st.session_state.selected_bike_meter)[0][0]['pointIndex']
+            
+            df_tranche_horaire = pd.DataFrame({"Tranche horaire":["Tranche 0","Tranche 1","Tranche 2","Tranche 3","Tranche 4","Tranche 5"],"Heures":["21h - 1h","5h - 8h","5h - 8h","9h - 12h","13h - 16h","17h - 20h"]}).set_index("Tranche horaire")
+            st.table(df_tranche_horaire.T)
+            
+            df_fig_train = RF_pred_train[RF_pred_train["id compteur"] == id_compteur]
+            df_fig_train["Tranche Horaire"] = (df_fig_train['Tranche horaire_1']*1 + df_fig_train['Tranche horaire_2']*2 + df_fig_train['Tranche horaire_3']*3 + df_fig_train['Tranche horaire_4']*4 + df_fig_train['Tranche horaire_5']*5)
+            df_fig_train["Date - Tranche Horaire"] = df_fig_train["Date"].astype(str) + " - " + df_fig_train["Tranche Horaire"].astype(str)
+            df_fig_train = df_fig_train.sort_values(by = ['Date',"Tranche Horaire"], ascending = True)
+        
+            
+            fig_RF_train = plot_prediction(df_fig_train['Date - Tranche Horaire'],
+                                              df_fig_train['prediction'], df_fig_train['Comptage Tranche Horaire'],
+                                              "Fitting sur le dataset d'entraînement")
+            st.write(fig_RF_train)
+        
+            df_fig_test = RF_pred_test[RF_pred_test["id compteur"] == id_compteur]
+            df_fig_test["Tranche Horaire"] = (df_fig_test['Tranche horaire_1']*1 + df_fig_test['Tranche horaire_2']*2 + df_fig_test['Tranche horaire_3']*3 + df_fig_test['Tranche horaire_4']*4 + df_fig_test['Tranche horaire_5']*5)
+            df_fig_test["Date - Tranche Horaire"] = df_fig_test["Date"].astype(str) + " - " + df_fig_test["Tranche Horaire"].astype(str)
+            df_fig_test = df_fig_test.sort_values(by = ['Date',"Tranche Horaire"], ascending = True)
+            
+         
+            fig_RF_test = plot_prediction(df_fig_test['Date - Tranche Horaire'],
+                                              df_fig_test['prediction'], df_fig_test['Comptage Tranche Horaire'],
+                                              "Prévisions sur le dataset de test")
+            st.write(fig_RF_test)
+            
+        
+
+    
---
+++ pages/03_📖_Notre projet en détails.py
@@ -0,0 +1,152 @@
+import streamlit as st
+import pandas as pd
+import gdown
+import plotly.express as px
+from streamlit_plotly_mapbox_events import plotly_mapbox_events
+from PIL import Image
+
+st.set_page_config(page_title = "Pycycle", layout="wide", initial_sidebar_state="collapsed", page_icon = ":bicyclist:")
+
+header = st.container()
+
+
+with header:
+    
+    @st.cache(allow_output_mutation=True)
+    def gdown_csv(url, separateur=";", index_colonne=0):
+        url = 'https://drive.google.com/uc?id=' + url.split('/')[-2]
+        output = "file.csv"
+        gdown.download(url, output, quiet=False) 
+        df = pd.read_csv(output, sep=separateur, index_col=index_colonne)
+        return df    
+
+    
+    
+    c1, c2 = st.columns((1, 1.2))
+    with c1:
+        st.title("Construction des modèles")
+        st.markdown("### Contexte du projet")
+        st.write("Ces dernières années, la politique cyclable de la mairie de Paris s'est montrée très ambitieuse, \
+                        notamment à travers le projet lancé en 2021 de rendre Paris 100% cyclable en 5 ans. \
+                        \n De nombreux travaux sont entrepris dans la capitale afin de créer des pistes cyclables, provoquant \
+                        une augmentation du trafic cycliste chaque année, lui qui a notamment explosé en 2020 avec le coronavirus \
+                                (*voir Pycycle > :rocket: Exploration des données*).\n")
+        st.write("\n L’objectif de ce projet est de prédire le trafic cycliste à Paris\
+                à partir de données issues de compteurs à vélos situés un peu\
+                partout à Paris.\n\
+                Plus précisément, on cherche à prédire le nombre de vélos circulant\
+                comptabilisé soit pour une agrégation de tous les compteurs\
+                (trafic général dans Paris), soit pour chacun des compteurs\
+                (localisé géographiquement) sur une période de temps définie.")
+    with c2:
+        plan_velos = Image.open("pages/Plan Vélo 2021 2026.jpeg")
+        st.image(plan_velos, use_column_width=True, caption = "Le Plan Vélo 2021-2026 de la Mairie de Paris")
+    
+    st.markdown('### Jeu de données initial')
+    st.write("Le dataset original sur lequel est basé notre projet est issu\
+            de l’[Open Data de la Mairie de Paris](https://opendata.paris.fr/explore/dataset/comptage-velo-donnees-compteurs/information/?disjunctive.id_compteur&disjunctive.nom_compteur&disjunctive.id&disjunctive.name)\
+            et présente des données de comptages horaires du trafic cycliste\
+            sur 13 mois glissants. La structure des données est la suivante :")
+            
+    df_format_data = pd.DataFrame({"Champs":["Identifiant du compteur",
+                                   "Nom du compteur : données complètes comportant l’adresse postale et le sens de comptage",
+                                   "Identifiant du site de comptage",
+                                   "Nom du site de comptage",
+                                   "Comptage horaire : le volume de cycliste en une heure --→ variable cible",
+                                   "Date et heure de comptage : données complètes comporte la date et l’heure du comptage horaire",
+                                   "Date d'installation du site de comptage",
+                                   "Lien vers photo du site de comptage",
+                                   "Coordonnées géographiques : coordonnées au format GPS du compteur",
+                                   "Identifiant technique compteur"]})
+    st.table(df_format_data)
+            
+    st.write("Nous avons également récupéré les données des années précédentes\
+            jusqu'en 2018 également disponible sur l’Open Data de la Mairie de Paris.\
+            Le jeu de données est alors composé d’environ _2 500 000 lignes_.")
+    
+    st.markdown('### Sélection des compteurs')
+    df_compteurs_selection = gdown_csv("https://drive.google.com/file/d/1wJvqoWAaCG4x-UDNPppiva47vnsQnV0F/view?usp=sharing")
+    df_compteurs_all = gdown_csv("https://drive.google.com/file/d/1JQOWtGU12_70PQcs0ayBgYs0-qPbZNqZ/view?usp=sharing")
+    df_compteurs_all = df_compteurs_all.merge(df_compteurs_selection, how="outer")
+    df_compteurs_all.loc[df_compteurs_all["id compteur"].notnull(),"selection"] = "Selectionnés"
+    df_compteurs_all.loc[df_compteurs_all["id compteur"].isna(),"selection"] = "Ecartés"
+    df_compteurs_all["size"]=5
+    
+    token = "pk.eyJ1IjoiY2FtaWxpYWIiLCJhIjoiY2w3a284am1uMDg5ejNvdDV6cWNzdTFsaSJ9.7nOYlVU0P_oLhl-7BnIu6Q"
+
+    st.write("Le dataset original (sur l’année 2021) permet d’identifier près d'une centaine\
+            compteurs dans Paris. Néanmoins, certains compteurs ont été enlevés,\
+            ajoutés ou remplacés au fil du temps depuis 2019. Afin d’avoir des\
+            observations cohérentes au fil du temps, il est nécessaire de\
+            ne conserver que les compteurs présents en 2019 et toujours présents\
+            aujourd’hui.")
+    st.write("Nous avons donc appliqué un filtre pour ne retenir que les capteurs remplissant cette condition:")
+
+    fig = px.scatter_mapbox(df_compteurs_all, 
+                            lat="Lat",
+                            lon="Long", 
+                            size="size",
+                            color="selection",
+                            hover_name = "Nom du compteur",
+                            size_max=10,
+                            zoom=11)
+
+    fig.update_layout(mapbox_style="light", mapbox_accesstoken=token)
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.update_layout(height = 400)
+    fig.update_coloraxes(showscale=False)
+
+    mapbox_events = plotly_mapbox_events(
+                                        fig,
+                                        click_event=True,
+                                        select_event=False,
+                                        hover_event=False,
+                                        relayout_event=False,
+                                        override_width = "100%",
+                                        key = 'selected_bike_meter')
+    
+    st.markdown('### Enrichissement des données')
+    
+    st.markdown("**Les données initiales permettent de renseigner les comptages en fonction :**")
+    st.markdown("- De la temporalité connu avec l'horodatage du comptage (jour de la semaine, mois, année)")
+    st.markdown("- De la géographie avec la position des compteurs (coordonnées GPS) et la direction du comptage")
+    st.markdown("")
+    st.markdown("\n  ")
+    st.markdown("**Le trafic cycliste est également influencé par d'autres facteurs que nous avons intégrés au jeu de donnée :**")
+    st.markdown("- Un contexte calendaire : vacances scolaires, jours fériés récupérés sur l'[Open Data du ministère de l'Education Nationale](https://data.education.gouv.fr/explore/dataset/fr-en-calendrier-scolaire/information/?disjunctive.description&disjunctive.population&disjunctive.location&disjunctive.zones&disjunctive.annee_scolaire&sort=end_date&calendarview=month)")
+    st.markdown("- Des évènements particuliers : confinement (ajout manuel du 17/03/2020 au 10/05/2020), Grève importante des transports publics (ajout manuel pour le mois de décembre 2019)")
+    st.markdown("- La météo : opinion météo de la journée, température moyenne, précipitation récupérés en [Open Data](https://www.historique-meteo.net/france/ile-de-france/paris/)")
+    st.markdown("- Des aménagements cyclables autour de chaque compteur, dont la longueur cumulée des pistes cyclables dans un rayon d'environ 1 km a été calculée à partir des données de l'[Open Data de la Ville de Paris](https://opendata.paris.fr/explore/dataset/reseau-cyclable/information/?disjunctive.typologie_simple&disjunctive.bidirectionnel&disjunctive.statut&disjunctive.sens_velo&disjunctive.voie&disjunctive.arrdt&disjunctive.bois&disjunctive.position&disjunctive.circulation&disjunctive.piste&disjunctive.couloir_bus&disjunctive.type_continuite&disjunctive.reseau)")
+    st.markdown("- Des Points d'Intérêt (musée, sites touristiques,...) autour de chaque compteur récupérés sur l'[Open Data de la Région Ile-de-France](https://data.iledefrance.fr/explore/dataset/principaux-sites-touristiques-en-ile-de-france0/information/)")
+    st.markdown("\n")
+    st.info("""Nous avons également introduit une notion d'**historique** en récupérant le comptage horaire des périodes précédentes. 
+    Cet enrichissement des données a fortement amélioré les performances de l'ensemble des modèles testés (_voir partie suivante_).  
+    \n On peut donc en déduire que **l'état du trafic cycliste à un instant T dépend principalement de l'état du trafic aux instants précédents**.""", icon='💡')
+    st.markdown("\n")
+    
+    st.markdown('### Les différents modèles testés')
+    
+    st.write("Dans un premier temps, nous nous sommes concentrés\
+            sur la prédiction du trafic des vélos à une échelle agrégée pour\
+            tous les compteurs (trafic général dans Paris, sans distinction géographique).")
+    st.write("Dans un second temps, nous avons commencé à construire des\
+            modélisations afin de réaliser une prédiction du trafic des vélos par capteur (localisé\
+            géographiquement) sur une période de temps définie.")
+    st.write("Différents modèles de régression ont été testés et enrichis par itération\
+            ainsi que des modèles de classification.\
+            Finalement, ce sont les modèles de régression **Random Forest** avec lesquels nous avons obtenu\
+            les meilleurs résultats.")
+    schema_modeles = Image.open("pages/shema_evolution_modelisation.png")
+    st.image(schema_modeles, use_column_width=True)
+    
+
+    st.markdown('### Bilan & Perspectives')
+    st.markdown("""La prise en compte de l’historique du comptage associé au modèle de Random Forest donnent des résultats plutôt bons avec un $r^2$ \
+        aux alentours de 0.92 pour le modèle global et le modèle par tranche horaire et capteur.""")
+    st.markdown("Cependant nous avons été limités par un manque de puissance de calcul mais également une instabilité des compteurs au fil des années. ")
+    st.markdown("Afin d’améliorer notre projet, il serait pertinent de mettre en place une pipeline de récupération des données pour avoir les données \
+        les plus récentes possibles. En effet, comme vu précedemment, la prédiction du trafic cycliste à un instant T dépend énormément du trafic aux instants \
+        précédents. La mise en place d’une base de données serait également un plus pour mieux gérer la volumétrie des données \
+        (près de *3 000 000 lignes*). ")
+    st.markdown("Enfin, une puissance de calcul plus importante (via un abonnement cloud par exemple) nous permettrait de \
+        construire un modèle horaire par capteurs.")
